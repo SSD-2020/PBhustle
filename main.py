386696b52bee7adb544dc07a6dd46cf04d93d7e2
Deepanshu Kumar Pali <deepanshukumarpali7@gmail.com>
2020-10-14 02:46:25 +0530

    added CFuser

--- main.py
+++ main.py
@@ -2,23 +2,6 @@
 from collections import defaultdict
 import requests
 
-class Codeforce:
-
-    def __init__(self,user_id):
-
-        self.user_id=user_id
-        self.user_info=[]
-        self.user_contests=[]
-
-
-    def get_info(self):
-
-        pass
-
-    def get_contests(self):
-
-        pass
-
 
 class Codechef:
 
@@ -32,7 +15,7 @@ class Codechef:
 
     def fetch_data(self):
 
-        url='https://competitive-coding-api.herokuapp.com/api/codechef/'+self.user_id
+        url='https://competitive-coding-api.herokuapp.com/api/codechef/' + self.user_id
         content=requests.get(url).json()
 
         if(content['status']!='Success'):  return
@@ -60,15 +43,75 @@ class Codechef:
             self.user_contests.append(here)
 
 
-CC_user=Codechef('deepu217')
-CC_user.fetch_data()
 
-print(CC_user.user_valid)
-print()
+class Codeforces:
 
-print(CC_user.user_info)
-print()
+    def __init__(self,user_id):
 
-print(CC_user.user_contests)
-print()
+        self.user_id=user_id
+        self.user_info={}
+        self.user_contests=[]
+        self.user_valid=False
 
+
+    def fetch_data(self):
+
+
+        # USER INFORMATION
+
+        url= "https://codeforces.com/api/user.info?handles=" + self.user_id
+        r=requests.get(url)
+        data_codeforces=r.json()
+
+        if(data_codeforces['status']!='OK'): return
+        self.user_valid=True
+        
+        self.user_info=data_codeforces['result'][0]
+
+
+        # USER CONTESTS
+
+        url=" https://codeforces.com/api/user.rating?handle=" + self.user_id
+        r=requests.get(url)
+        contests=r.json()
+
+        self.user_contests=contests['result']
+
+        for data in self.user_contests:
+
+            del data['ratingUpdateTimeSeconds']
+            del data['oldRating']
+            del data['handle']
+
+
+
+
+
+
+
+# CC_user=Codechef('deepu217')
+# CC_user.fetch_data()
+
+# print(CC_user.user_valid)
+# print()
+
+# print(CC_user.user_info)
+# print()
+
+# print(CC_user.user_contests)
+# print()
+
+
+
+
+# CF_user=Codeforces('deepanshu_pal')
+# CF_user.fetch_data()
+
+# print(CF_user.user_valid)
+# print()
+
+# print(CF_user.user_info)
+# print()
+
+# print(CF_user.user_contests)
+# print()
