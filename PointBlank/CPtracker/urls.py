afc6e88c87a7d2b7e832fcbc5b731d81d95f9336
Deepanshu Kumar Pali <deepanshukumarpali7@gmail.com>
2020-10-14 01:34:28 +0530

    added classes

--- PointBlank/CPtracker/urls.py
+++ PointBlank/CPtracker/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf import settings
 
 
 urlpatterns= [
-    path('',views.user_home,name='user_home'),
+    path('hi',views.user_home,name='user_home'),
     # path('virtual',views.virtual,name='virtual')
 
 ]
