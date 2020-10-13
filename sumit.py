c487da7907b5743e58b5511e26deba3f969792a9
SUMIT KUMAR <suanmj18899@gmail.com>
2020-10-14 01:36:22 +0530

    sumit's

---
+++ sumit.py
@@ -0,0 +1,19 @@
+#function to extract the codeforces user data
+import requests
+
+def Codeforces_User():
+
+    url= "https://codeforces.com/api/user.info?handles=sumitthakur"
+    r=requests.get(url)
+
+    data_codeforces=r.json()
+
+    if(data_codeforces['status']!='OK'): return -1
+
+    data_codeforces=data_codeforces['result'][0]
+
+    print(data_codeforces)
+    #return data_codeforces
+
+def Codeforces_contests():
+    
