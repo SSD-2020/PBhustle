2cc6c6fceb632ac0b3389633801d9d545924c480
Sparsh Kesari <39815871+SparshKesari@users.noreply.github.com>
2020-10-14 22:15:37 +0530

    ratingpage

--- PointBlank/CPtracker/urls.py
+++ PointBlank/CPtracker/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings
 urlpatterns= [
     path('',views.userhome,name='userhome'),
     path('codeforces/', views.codeforces , name='codeforces'),
+    path('codeforcesrating/', views.codeforcesrating , name='codeforcesrating'),
     path('codechef/', views.codechef , name='codechef'),
     path('hackerrank/', views.hackerrank, name='hackerrank'),
     path('pbhustle/', views.pbhustle , name='pbhustle'),
