a0a4ef7c9599c1e293afb6ff6741b249f0bd8c00
Sparsh Kesari <39815871+SparshKesari@users.noreply.github.com>
2020-10-25 16:49:37 +0530

    Revert "error list"
    This reverts commit dd380c23f1592051401ce922a38872f51f772b5c.

--- ErrorsToCatch.md
+++
@@ -1,5 +0,0 @@
-1. Error 404 : Ramdom URL
-2. When Pb Huslte rating is -10000 and open PbHustle
-3. Connection Error
-4. Api not responing
-5. Api taking more time that expected . Connection Time Out ~30seconds.
--- PointBlank/CPtracker/urls.py
+++ PointBlank/CPtracker/urls.py
@@ -3,23 +3,20 @@ from . import views
 from django.conf import settings
 
 
-urlpatterns = [
+urlpatterns= [
 
-    path('', views.landingpage, name='landingpage'),
-    path('signin/', views.signin, name='signin'),
-    path('signup/', views.signup, name='signup'),
-    path('edit/', views.edit, name='edit'),
-    path('logout/', views.logout, name='logout'),
-    path('userhome/', views.userhome, name='userhome'),
-    path('userhome/update/', views.update, name='update'),
+    path('',views.landingpage,name='landingpage'),
+    path('signin/',views.signin,name='signin'),
+    path('signup/',views.signup,name='signup'),
+    path('edit/',views.edit,name='edit'),
+    path('logout/',views.logout,name='logout'),
+    path('userhome/',views.userhome,name='userhome'),
+    path('userhome/update/',views.update,name='update'),
     path('userhome/codeforces/', views.codeforces, name='codeforces'),
-    path('userhome/codeforces/codeforcesCompare/',
-         views.codeforcesCompare, name='codeforcesCompare'),
-    path('userhome/pbhustle/pbhustleCompare/',
-         views.pbhustleCompare, name='pbhustleCompare'),
-    path('userhome/codechef/', views.codechef, name='codechef'),
-    path('userhome/codechef/codechefCompare/',
-         views.codechefCompare, name='codechefCompare'),
-    path('userhome/pbhustle/', views.pbhustle, name='pbhustle'),
+    path('userhome/codeforces/codeforcesCompare/', views.codeforcesCompare, name='codeforcesCompare'),
+    path('userhome/pbhustle/pbhustleCompare/', views.pbhustleCompare, name='pbhustleCompare'),
+    path('userhome/codechef/', views.codechef , name='codechef'),
+    path('userhome/codechef/codechefCompare/', views.codechefCompare, name='codechefCompare'),
+    path('userhome/pbhustle/', views.pbhustle , name='pbhustle'),
 
 ]
