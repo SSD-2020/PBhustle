dce5664d7cced31662848a3360571fac953f35ef
Sparsh Kesari <39815871+SparshKesari@users.noreply.github.com>
2020-10-28 19:31:26 +0530

    deploy

--- PointBlank/PointBlank/settings.py
+++ PointBlank/PointBlank/settings.py
@@ -10,8 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+
 from pathlib import Path
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -123,3 +126,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+
+django_heroku.settings(locals())
